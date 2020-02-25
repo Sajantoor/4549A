54e57427214a1118756e5c8127b7bf7e27c9912f
Sajantoor <sajantoor98@gmail.com>
2020-02-25 13:10:48 -0800

    Redid vision sensor values

--- include/vision.h
+++ include/vision.h
@@ -18,8 +18,8 @@ void visionMovement(void*ignore);
 extern pros::task_t vision_task;
 extern pros::task_t visionMovement_task;
 extern data currentCube;
-const int CUBE_SIZE_THRESHOLD_MIN = 2000; // make bigger if detecting unwanted cubes, smaller if not picking up cubes
-const int CUBE_SIZE_THRESHOLD_MAX = 5000; // make smaller if not intaking cubes
-const int CENTER_X = -55;
+const int CUBE_SIZE_THRESHOLD_MIN = 6708; // make bigger if detecting unwanted cubes, smaller if not picking up cubes
+const int CUBE_SIZE_THRESHOLD_MAX = 6000; // make smaller if not intaking cubes
+const int CENTER_X = -70;
 // extern pros::vision_object_s_t;
 #endif
